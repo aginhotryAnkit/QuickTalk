7ac0abff0cb9845f9fd47b36ed5b71fe3c70c479
ankit <ankitaginhotry@makewebbetter.com>
2025-02-06 18:18:47 +0530

    login and singup pages added in view section

--- app.js
+++ app.js
@@ -2,13 +2,27 @@ const express = require('express');
 const http = require('http');
 const io = require('socket.io');
 const app = express();
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+
+// Middleware to parse URL-encoded bodies (like form data)
+app.use(express.urlencoded({ extended: true }));
 
 
 require('dotenv').config();
+
+//middlewares
 app.use(express.json());
+app.use(express.static('public'));
+
+//app configuration
+app.set('view engine', 'ejs');
+app.set('views', './src/views/pages');
+
 
 const server = http.createServer(app);
 const socketIo = io(server);
+
 
 socketIo.on("connection", (socket)=>{
     console.log("New Connection");
@@ -33,4 +47,22 @@ app.get("/check", (req, res)=>{
 
 server.listen(process.env.PORT, (req, res)=>{
     console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+
+//other parts
+app.get("/", (req, res)=>{
+    res.render('register');
+});
+
+
+// register a user 
+app.post("/v1/register", (req, res)=>{
+    const { email, password, confirmPassword } = req.body;
+    res.send(req.body);
+});
+
+
+app.get("/login", (req, res)=>{
+    res.render('login');
 });
