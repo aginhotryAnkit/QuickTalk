65f47de78eb4da0904358609bb44bfc85f2bc8e7
ankit <ankitaginhotry@makewebbetter.com>
2025-02-10 12:27:15 +0530

    complate: user reagistration route complated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,14 +1,52 @@
-function registerUser(req, res){
-    //complete the code
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const Joi = require('joi');
 
-    confirm('controller register');
 
-    res.send("hello world");
+const userValidation = Joi.object({
+    firstName: Joi.string(),
+    lastName: Joi.string(), 
+    email: Joi.string().email().required(),
+    password:Joi.string().min(8).max(20).required(),
+    confirmPassword:Joi.string().valid(Joi.ref('password')).required()
+});
+
+async function registerUser(req, res) {
+  try {
+    const { error } = userValidation.validate(req.body);
+
+    if (error) {
+        return res.status(400).json({ success:false, error: error.details[0].message });
+    }
+
+      // Create a new user
+    const {firstName, lastName, email, password, confirmPassword} = req.body;
+
+    if(password!==confirmPassword){
+        res.status(400).json({success:false, error: "Password does not match"});
+    }
+
+    //encrypt_password using brypt
+    let salt = bcrypt.genSaltSync(10);
+    let hashPassword = bcrypt.hashSync(password, salt);
+
+    let userResponse = await User.create({
+        firstName: firstName,
+        lastName: lastName, 
+        email : email,
+        password: hashPassword
+    });
+
+    res.status(201).json({ success: true, message: "User registered successfully"});
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success:false, message: "Server error", error: error.message });
+  }
 }
 
-function loginUser(){
-    //complete the code
-
+function loginUser(req, res) {
+  // Implement login logic here
 }
 
-module.exports = {registerUser, loginUser};
+module.exports = { registerUser, loginUser };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController.js");
 
-router.get("/register", registerUser);
-router.get("/login", loginUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
